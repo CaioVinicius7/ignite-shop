caa21bfaedb8f05593a3e93c68a3ac1d48ea616b
Caio Vinícius <caio1525pereira@gmail.com>
2022-09-08 12:52:28 -0300

    Estrutura visual - Cabeçalho da aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,19 @@
 import { AppProps } from "next/app";
 
+import logoImg from "../assets/logo.svg";
 import { globalStyles } from "../styles/global";
-
-globalStyles();
+import { Container, Header } from "../styles/pages/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  globalStyles();
+
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="Logo" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  );
 }
