4a0852a86c9938cddc23709890a7ae3bb08eb3d0
Caio Vinícius <caio1525pereira@gmail.com>
2022-09-09 12:04:53 -0300

    Integração com Stripe - Utilizando SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { useKeenSlider } from "keen-slider/react";
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Image from "next/future/image";
 import Stripe from "stripe";
 
@@ -48,7 +48,7 @@ export default function Home({ products }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ["data.default_price"]
   });
@@ -67,6 +67,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       products
-    }
+    },
+    revalidate: 60 * 60 * 2 // 2 hours
   };
 };
