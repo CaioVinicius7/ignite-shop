810530ee681f1467ac2663bbd021df2b904f4d1b
Caio Vinícius <caio1525pereira@gmail.com>
2022-09-15 19:21:27 -0300

    Produto & Checkout - Estrutura de sucesso

--- src/pages/success/index.tsx
+++ src/pages/success/index.tsx
@@ -1,3 +1,21 @@
+import Link from "next/link";
+
+import { ImageContainer, SuccessContainer } from "../../styles/pages/success";
+
 export default function Success() {
-  return <h1> Success </h1>;
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer></ImageContainer>
+
+      <p>
+        Uhuul <strong>Caio Vinícius</strong>, sua
+        <strong> camiseta Beyond the Limits </strong> já está a caminha da sua
+        casa.
+      </p>
+
+      <Link href="/">Voltar ao catálogo</Link>
+    </SuccessContainer>
+  );
 }
